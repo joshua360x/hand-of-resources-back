952697b1a02aef56562192eba7e0b551a20f6f13
joshua360x <jkw5499@psu.edu>
2022-03-18 14:01:54 -0400

    added GET REQUEST for particular Car

--- __tests__/car.test.js
+++ __tests__/car.test.js
@@ -3,8 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-
-
 describe('hand-resources routes', () => {
   beforeAll(() => {
     return setup(pool);
@@ -13,9 +11,6 @@ describe('hand-resources routes', () => {
   afterAll(() => {
     pool.end();
   });
-
-
-
 
   it('creates a Car', async () => {
     const expected = {
@@ -27,15 +22,19 @@ describe('hand-resources routes', () => {
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
 
-
   it('gets all cars', async () => {
-
     const res = await request(app).get('/api/v1/cars');
-    expect(res.body).toEqual((expect.arrayContaining([])));
+    expect(res.body).toEqual(expect.arrayContaining([]));
   });
 
-
-
-
-
+  it('gets a specific car', async () => {
+    const res = await request(app).get('/api/v1/cars/1');
+    const expected = {
+      id: '1',
+      make: 'ford',
+      model: 'fusion',
+      year: 2000,
+    };
+    expect(res.body).toEqual(expected);
+  });
 });
